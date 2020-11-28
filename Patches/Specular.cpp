8098868ac809f5ed7869e80cef8da54c50dd1127
FrozenFish24 <xfrozenfishx@gmail.com>
2020-11-29 05:42:15 +0800

    Fill out ModelId enum Add isJames, isMaria functions Set Maria specularity to 10% if no flashlight

--- Patches/Specular.cpp
+++ Patches/Specular.cpp
@@ -6,10 +6,32 @@
 
 enum class ModelId
 {
-	James = 257,
-	MariaSH2 = 262,
-	MariaBFAW = 270,
-	MariaRevolver = 2057,
+	hhh_mar = 262,
+	//hhh_mar_notex = 262,
+	lll_mar = 261,
+	rhhh_mar = 294,
+	//rhhh_mar_notex = 294,
+	rlll_mar = 293,
+	lxx_mar = 270,
+	rlxx_mar = 302,
+	//rlxx_mar_notex = 302,
+
+	hhh_jms = 259,
+	//hhh_jms_notex = 259,
+	hhl_jms = 258,
+	//hhl_jms_notex = 258,
+	hll_jms = 257,
+	//hll_jms_notex = 257,
+	lll_jms = 256,
+	//lll_jms_hand = 256,
+	//lll_jms_kaku = 256,
+	//lll_jms_n = 256,
+	//lll_jms_normal = 256,
+	//lll_jms_notex = 256,
+	rhhh_jms = 291,
+	rhhl_jms = 290,
+	rhll_jms = 289,
+	rlll_jms = 288
 };
 
 struct LightSource
@@ -27,6 +49,40 @@ static auto getModelId_50B6C0 = reinterpret_cast<ModelId(__cdecl*)()>(0x50B6C0);
 static auto getLightSourceCount_50C590 = *reinterpret_cast<int(__cdecl*)()>(0x50C590);
 static auto getLightSourceStruct_50C5A0 = *reinterpret_cast<LightSource * (__cdecl*)(int)>(0x50C5A0);
 static auto& pD3DDevice_A32894 = *reinterpret_cast<IDirect3DDevice8**>(0xA32894);
+
+bool isJames(ModelId id)
+{
+	switch (id)
+	{
+	case ModelId::hhh_jms:
+	case ModelId::hhl_jms:
+	case ModelId::hll_jms:
+	case ModelId::lll_jms:
+	case ModelId::rhhh_jms:
+	case ModelId::rhhl_jms:
+	case ModelId::rhll_jms:
+	case ModelId::rlll_jms:
+		return true;
+	}
+
+	return false;
+}
+
+bool isMaria(ModelId id)
+{
+	switch (id)
+	{
+	case ModelId::hhh_mar:
+	case ModelId::lll_mar:
+	case ModelId::rhhh_mar:
+	case ModelId::rlll_mar:
+	case ModelId::lxx_mar:
+	case ModelId::rlxx_mar:
+		return true;
+	}
+
+	return false;
+}
 
 int __cdecl Part1()
 {
@@ -77,26 +133,35 @@ HRESULT __stdcall Part3(IDirect3DDevice8* /*This*/, DWORD Register, void* pConst
 	auto constants = reinterpret_cast<float*>(pConstantData);
 	if (constants[0] != 0.0f || constants[1] != 0.0f || constants[2] != 0.0f)
 	{
-		if (getModelId_50B6C0() == ModelId::James && inSpecialLightZone)
+		ModelId modelId = getModelId_50B6C0();
+
+		if (isJames(modelId) && inSpecialLightZone)
 		{
 			// 75% if we're James and in a special lighting zone
 			constants[0] = 0.75f;
 			constants[1] = 0.75f;
 			constants[2] = 0.75f;
 		}
-		else if (getModelId_50B6C0() != ModelId::James && inSpecialLightZone)
+		else if (!isJames(modelId) && !isMaria(modelId) && inSpecialLightZone)
 		{
-			// 50% If we're not James and we're in a special lighting zone
+			// 50% If we're not James or Maria and we're in a special lighting zone
 			constants[0] = 0.50f;
 			constants[1] = 0.50f;
 			constants[2] = 0.50f;
 		}
-		else if (getModelId_50B6C0() != ModelId::James && !inSpecialLightZone && !useFakeLight)
+		else if (!isJames(modelId) && !isMaria(modelId) && !inSpecialLightZone && !useFakeLight)
 		{
-			// 50% If we're not James and the flashlight is on
+			// 50% If we're not James or Maria and the flashlight is on
 			constants[0] = 0.50f;
 			constants[1] = 0.50f;
 			constants[2] = 0.50f;
+		}
+		else if (isMaria(modelId) && !inSpecialLightZone && useFakeLight)
+		{
+			// 10% If we're Maria and the flashlight is off
+			constants[0] = 0.10f;
+			constants[1] = 0.10f;
+			constants[2] = 0.10f;
 		}
 		else
 		{

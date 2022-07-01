8da6e5e92e88a7687ffdee7ca5ce1e095ecf010b
Elisha Riedlinger <elisha@novicemail.com>
2022-07-01 08:39:51 -0700

    Update version and build number

--- Resources/Resource.h
+++ Resources/Resource.h
@@ -18,8 +18,8 @@
 
 // Main resource file details
 #define APP_NAME				"Silent Hill 2: Enhanced Edition"
-#define APP_MAJOR				1
-#define APP_MINOR				8
+#define APP_MAJOR				2
+#define APP_MINOR				0
 #define APP_BUILDNUMBER			BUILD_NUMBER
 #define APP_REVISION			0
 #define APP_COMPANYNAME			"Sadrate Presents"

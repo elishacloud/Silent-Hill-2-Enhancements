cd0ab0a4d5d4ec19273c3d200c726474d8ffc3d3
mercury501 <tommaso.desoldaee@gmail.com>
2022-09-22 19:21:36 +0200

    initial mouse reversing

--- Wrappers/dinput8/InputTweaks.h
+++ Wrappers/dinput8/InputTweaks.h
@@ -11,10 +11,14 @@ private:
 
 	bool IsKeyPressed(int keyIndex);
 	void ClearKey(int keyIndex);
+	int32_t GetMouseRelXChange();
+	int32_t GetMouseRelYChange();
+	int isMouseButtonPressed(int buttonIndex);
 
 public:
 	void SetKeyboardInterfaceAddr(LPDIRECTINPUTDEVICE8A ProxyInterface);
 	void SetMouseInterfaceAddr(LPDIRECTINPUTDEVICE8A ProxyInterface);
+	void RemoveAddr(LPDIRECTINPUTDEVICE8A ProxyInterface);
 	
 	void TweakGetDeviceState(LPDIRECTINPUTDEVICE8A ProxyInterface, DWORD cbData, LPVOID lpvData);
 	void TweakGetDeviceData(LPDIRECTINPUTDEVICE8A ProxyInterface, DWORD cbObjectData, LPDIDEVICEOBJECTDATA rgdod, LPDWORD pdwInOut, DWORD dwFlags);

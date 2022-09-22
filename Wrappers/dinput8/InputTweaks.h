75c779a2aaee588fa1460528318fd5be80f683a0
mercury501 <tommaso.desoldaee@gmail.com>
2022-09-22 12:47:38 +0200

    Keyboard input handling

---
+++ Wrappers/dinput8/InputTweaks.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "dinput8wrapper.h"
+
+class InputTweaks
+{
+private:
+	LPDIRECTINPUTDEVICE8A KeyboardInterfaceAddress = nullptr;
+	LPDIRECTINPUTDEVICE8A MouseInterfaceAddress = nullptr;
+
+public:
+	void SetKeyboardInterfaceAddr(LPDIRECTINPUTDEVICE8A ProxyInterface);
+	void SetMouseInterfaceAddr(LPDIRECTINPUTDEVICE8A ProxyInterface);
+	
+	void TweakGetDeviceState(LPDIRECTINPUTDEVICE8A ProxyInterface, DWORD cbData, LPVOID lpvData);
+	void TweakGetDeviceData(LPDIRECTINPUTDEVICE8A ProxyInterface, DWORD cbObjectData, LPDIDEVICEOBJECTDATA rgdod, LPDWORD pdwInOut, DWORD dwFlags);
+};

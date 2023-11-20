8a897b1068f9a346406facadb51295c4cef395c1
Elisha Riedlinger <elisha@novicemail.com>
2023-11-20 09:37:38 -0800

    Add CanUseFlashlight2
    #834

--- Patches/PlayWavSound.cpp
+++ Patches/PlayWavSound.cpp
@@ -30,6 +30,11 @@ void RunPlayAdditionalSounds()
 		GameVersion == SH2V_11 ? (BYTE*)0x01F8182C :
 		GameVersion == SH2V_DC ? (BYTE*)0x01F8082C : nullptr;
 
+	static BYTE* CanUseFlashlight2 =
+		GameVersion == SH2V_10 ? (BYTE*)0x01FB7F0D :
+		GameVersion == SH2V_11 ? (BYTE*)0x01FBBB0D :
+		GameVersion == SH2V_DC ? (BYTE*)0x01FBAB0D : nullptr;
+
 	static BYTE* WorldColorR =
 		GameVersion == SH2V_10 ? (BYTE*)0x00942C50 :
 		GameVersion == SH2V_11 ? (BYTE*)0x00946850 :
@@ -68,7 +73,7 @@ void RunPlayAdditionalSounds()
 
 	// Check for flashlight on/off
 	if (LastFlashLightSwitch != FlashLightSwitch && !IsBossLevel && (
-		(*CanUseFlashlight == 1 && (RoomID != 0x24 /*Angela apt room*/ && RoomID != 0x89 /*Maria in prison*/ && RoomID != 0x8F /*Eddie boss room 1*/ && RoomID != 0x90 /*Eddie boss room 2*/ && RoomID != 0xA2 /*Hotel room 302*/)) ||
+		((*CanUseFlashlight == 1 && *CanUseFlashlight2 == 1) && (RoomID != 0x24 /*Angela apt room*/ && RoomID != 0x89 /*Maria in prison*/ && RoomID != 0x8F /*Eddie boss room 1*/ && RoomID != 0x90 /*Eddie boss room 2*/ && RoomID != 0xA2 /*Hotel room 302*/)) ||
 		((RoomID == 0x04 /*Town East*/ || RoomID == 0x08 /*Town West*/) && (*WorldColorR == 0 && *WorldColorG == 0 && *WorldColorB == 0)) ||
 		(RoomID == 0xA2 /*Hotel room 302*/ && *InventoryItem < 0x80 /*VHSTape is NOT in player's inventory*/)))
 	{

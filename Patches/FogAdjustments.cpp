6d2628eebaa0a1cb7b07058771041733b762abcc
Elisha Riedlinger <elisha@novicemail.com>
2024-04-13 22:36:53 -0700

    Add back Blue Creek Fog Adjustment ASM

--- Patches/FogAdjustments.cpp
+++ Patches/FogAdjustments.cpp
@@ -21,15 +21,56 @@
 #include "Logging\Logging.h"
 
 // Variables for ASM
-void *NewEnvFogRGB;
-void *OriginalEnvFogRGB;
-void *jmpFinalAreaBossAddr1;
-void *jmpFinalAreaBossAddr2;
+float* FogFrontPointer;
+void* FogBackPointer;
+void* jmpBlueCreekFogReturnAddr;
+void* NewEnvFogRGB;
+void* OriginalEnvFogRGB;
+void* jmpFinalAreaBossAddr1;
+void* jmpFinalAreaBossAddr2;
+BYTE LastFlashLightMode;
 
 // Fog values
+constexpr float NewFrontFog = 2200.0f;
+constexpr float NewBackFog = 2800.0f;
+constexpr float OriginalFrontFog = 8000.0f;
+constexpr float OriginalBackFog = 12000.0f;
 constexpr float BlueCreekNewFog = 4200.0f;
 constexpr float BlueCreekOriginalFog = 3000.0f;
 constexpr float FinalAreaCameraYOrientation = -15750.0f;
+
+// ASM functions to adjust fog values for Blue Creek Apt Room 209
+__declspec(naked) void __stdcall BlueCreekFogAdjustmentASM()
+{
+	__asm
+	{
+		push eax
+		push ecx
+		mov eax, dword ptr ds : [RoomIDAddr]
+		cmp dword ptr ds : [eax], R_APT_W_RM_208_209
+		jne near ConditionsNotMet						// jumps if not Blue Creek Apt Room 209
+		mov ecx, dword ptr ds : [FlashlightSwitchAddr]
+		cmp dword ptr ds : [ecx], 0
+		je near ConditionsNotMet						// jumps if flashlight is off
+		mov eax, dword ptr ds : [LastFlashLightMode]
+		cmp eax, dword ptr ds : [ecx]
+		je near ConditionsNotMet						// jumps if flashlight mode has not changed
+		mov ecx, BlueCreekNewFog
+		mov eax, dword ptr ds : [FogFrontPointer]
+		mov dword ptr ds : [eax], ecx					// new fog value
+		jmp near ExitFunction
+
+	ConditionsNotMet:
+		mov ecx, BlueCreekOriginalFog
+		mov eax, dword ptr ds : [FogFrontPointer]
+		mov dword ptr ds : [eax], ecx					// original fog value; 3000.0f flt
+
+	ExitFunction:
+		pop ecx
+		pop eax
+		jmp jmpBlueCreekFogReturnAddr
+	}
+}
 
 // ASM final boss area 1
 __declspec(naked) void __stdcall FinalAreaBoss1ASM()
@@ -85,6 +126,42 @@ __declspec(naked) void __stdcall FinalAreaBoss2ASM()
 
 void PatchFogParameters()
 {
+	// Get Fog address
+	constexpr BYTE FogSearchBytes[]{ 0x8B, 0xF8, 0x81, 0xE7, 0xFF, 0x00, 0x00, 0x00, 0xC1, 0xE7, 0x10, 0x25, 0x00, 0xFF, 0x00, 0xFF, 0x0B, 0xF7, 0x0B, 0xF0, 0x56 };
+	DWORD FogAddr = SearchAndGetAddresses(0x00479E71, 0x0047A111, 0x0047A321, FogSearchBytes, sizeof(FogSearchBytes), 0x00, __FUNCTION__);
+
+	// Checking address pointer
+	if (!FogAddr)
+	{
+		Logging::Log() << __FUNCTION__ << " Error: failed to find memory address!";
+		return;
+	}
+	void* FogMemoryAddr = (void*)(FogAddr - 0x1A);
+
+	// Get Blue Creek return address
+	constexpr BYTE BlueCreekFogSearchBytes[]{ 0x85, 0xC0, 0xDF, 0xE0, 0x0F, 0x84, 0x90, 0x01, 0x00, 0x00, 0xF6, 0xC4, 0x44, 0x7A, 0x2A };
+	FogAddr = SearchAndGetAddresses(0x0047BE75, 0x0047C115, 0x0047C325, BlueCreekFogSearchBytes, sizeof(BlueCreekFogSearchBytes), 0x00, __FUNCTION__);
+
+	// Checking address pointer
+	if (!FogAddr)
+	{
+		Logging::Log() << __FUNCTION__ << " Error: failed to find memory address!";
+		return;
+	}
+	jmpBlueCreekFogReturnAddr = (void*)(FogAddr + 0x23);
+
+	// Check for valid code before updating
+	if (!CheckMemoryAddress(FogMemoryAddr, "\x8B\x0D", 2, __FUNCTION__) ||
+		!CheckMemoryAddress(jmpBlueCreekFogReturnAddr, "\xC7\x05", 2, __FUNCTION__))
+	{
+		Logging::Log() << __FUNCTION__ << " Error: memory addresses don't match!";
+		return;
+	}
+
+	// Fog front and back addresses
+	memcpy(&FogFrontPointer, (void*)((DWORD)FogMemoryAddr - 4), sizeof(DWORD));
+	FogBackPointer = (void*)((DWORD)FogFrontPointer - 4);
+
 	// New environment fog RGB
 	constexpr BYTE NewEnvFogSearchBytes[]{ 0x90, 0x90, 0x90, 0x8B, 0x44, 0x24, 0x04, 0x8B, 0x0D };
 	NewEnvFogRGB = (void*)ReadSearchedAddresses(0x004798ED, 0x00479B8D, 0x00479D9D, NewEnvFogSearchBytes, sizeof(NewEnvFogSearchBytes), 0x09, __FUNCTION__);
@@ -133,8 +210,11 @@ void PatchFogParameters()
 	// Get Camera in-game position Y
 	GetInGameCameraPosYPointer();
 
+	// Get flashlight pointer
+	GetFlashlightSwitchPointer();
+
 	// Checking address pointers
-	if (!RoomIDAddr || !CutsceneIDAddr || !CutscenePosAddr || !InGameCameraPosYAddr)
+	if (!RoomIDAddr || !CutsceneIDAddr || !CutscenePosAddr || !InGameCameraPosYAddr || !FlashlightSwitchAddr)
 	{
 		Logging::Log() << __FUNCTION__ << " Error: failed to get cutscene ID or position address!";
 		return;
@@ -142,6 +222,7 @@ void PatchFogParameters()
 
 	// Update SH2 code
 	Logging::Log() << "Updating Fog Parameters...";
+	WriteJMPtoMemory((BYTE*)((DWORD)jmpBlueCreekFogReturnAddr - 10), *BlueCreekFogAdjustmentASM, 10);
 	WriteJMPtoMemory((BYTE*)FinalBossAddr1, FinalAreaBoss1ASM);
 	WriteJMPtoMemory((BYTE*)FinalBossAddr2, FinalAreaBoss2ASM);
 }
@@ -149,7 +230,7 @@ void PatchFogParameters()
 // Slow the fog movement in certain areas of the game to better match the PS2's fog movements
 void RunFogSpeed()
 {
-	static float *FogSpeed = nullptr;
+	static float* FogSpeed = nullptr;
 	if (!FogSpeed)
 	{
 		RUNONCE();
@@ -163,35 +244,16 @@ void RunFogSpeed()
 		}
 	}
 
-	static float* FogFrontPointer = nullptr;
+	// Checking fog address pointer
 	if (!FogFrontPointer)
 	{
 		RUNONCE();
 
-		// Get Fog address
-		constexpr BYTE FogSearchBytes[]{ 0x8B, 0xF8, 0x81, 0xE7, 0xFF, 0x00, 0x00, 0x00, 0xC1, 0xE7, 0x10, 0x25, 0x00, 0xFF, 0x00, 0xFF, 0x0B, 0xF7, 0x0B, 0xF0, 0x56 };
-		DWORD FogAddr = SearchAndGetAddresses(0x00479E71, 0x0047A111, 0x0047A321, FogSearchBytes, sizeof(FogSearchBytes), 0x00, __FUNCTION__);
-
-		// Checking address pointer
-		if (!FogAddr)
-		{
-			Logging::Log() << __FUNCTION__ << " Error: failed to find memory address!";
-			return;
-		}
-		void* FogMemoryAddr = (void*)(FogAddr - 0x1A);
-
-		// Check for valid code before updating
-		if (!CheckMemoryAddress(FogMemoryAddr, "\x8B\x0D", 2, __FUNCTION__))
-		{
-			Logging::Log() << __FUNCTION__ << " Error: memory addresses don't match!";
-			return;
-		}
-
-		// Fog front and back addresses
-		memcpy(&FogFrontPointer, (void*)((DWORD)FogMemoryAddr - 4), sizeof(DWORD));
+		Logging::Log() << __FUNCTION__ << " Error: failed to find memory address!";
+		return;
 	}
 
-	static float *JamesFogInfluence = nullptr;
+	static float* JamesFogInfluence = nullptr;
 	if (!JamesFogInfluence)
 	{
 		RUNONCE();
@@ -248,6 +310,7 @@ void RunFogSpeed()
 	{
 		*FogFrontPointer = BlueCreekNewFog;
 	}
+	LastFlashLightMode = GetFlashlightSwitch();
 
 	// Prevents fog from "sticking" to James during certain parts of the Forest trail
 	static bool ValueSet = false;

d7d99140e16f0e3cd9c08c3a99868c1990c7f687
Elisha Riedlinger <elisha@novicemail.com>
2024-05-20 18:13:58 -0700

    Remove unneeded HangOnEsc fix

--- Patches/HangOnEsc.cpp
+++ Patches/HangOnEsc.cpp
@@ -113,12 +113,9 @@ void RunHangOnEsc()
 	RUNCODEONCE(Logging::Log() << "Fixing Esc while transition is active...");
 
 	// Prevent player from pressing Esc while transition is active
-	static DWORD FramesSinceTransition = 0;
-	bool IsInTransition = (GetTransitionState() == FADE_FROM_BLACK || (IsInBloomEffect && GetRoomID() == R_EDI_BOSS_RM_2) || IsInFakeFadeout);
-	if (*(DWORD*)Address != 0x03 && (IsInTransition || FramesSinceTransition < 3))
+	if (*(DWORD*)Address != 0x03 && (GetTransitionState() == FADE_FROM_BLACK || (IsInBloomEffect && GetRoomID() == R_EDI_BOSS_RM_2) || IsInFakeFadeout))
 	{
 		DWORD Value = 3;
 		UpdateMemoryAddress((void*)Address, &Value, sizeof(DWORD));
 	}
-	FramesSinceTransition = IsInTransition ? 0 : FramesSinceTransition + 1;
 }

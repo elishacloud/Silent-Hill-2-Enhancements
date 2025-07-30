0bf0ddc1f1347fd80f5674165c8a71f83cd46553
Elisha Riedlinger <elisha@novicemail.com>
2025-07-30 12:32:55 -0700

    Simplify LimitFrameRate()

--- Wrappers/d3d8/IDirect3DDevice8.cpp
+++ Wrappers/d3d8/IDirect3DDevice8.cpp
@@ -1635,50 +1635,44 @@ void m_IDirect3DDevice8::LimitFrameRate()
 	// Count the number of frames
 	Counter.FrameCounter++;
 
-	// Get performance frequency if not already cached
-	static LARGE_INTEGER Frequency = {};
-	if (!Frequency.QuadPart)
-	{
-		QueryPerformanceFrequency(&Frequency);
-	}
-	static LONGLONG TicksPerMS = Frequency.QuadPart / 1000;
+	// Get performance frequency once
+	static const LARGE_INTEGER Frequency = [] {
+		LARGE_INTEGER freq = {};
+		QueryPerformanceFrequency(&freq);
+		return freq;
+		}();
+	static const LONGLONG TicksPerMS = Frequency.QuadPart / 1000;
 
-	// Calculate the delay time in ticks
+	// Calculate time per frame in ticks
 	static long double PerFrameFPS = LimitPerFrameFPS;
-	static LONGLONG PreFrameTicks = static_cast<LONGLONG>(static_cast<long double>(Frequency.QuadPart) / PerFrameFPS);
+	static LONGLONG PerFrameTicks = static_cast<LONGLONG>(static_cast<long double>(Frequency.QuadPart) / PerFrameFPS);
 
-	// Get next tick time
+	// Get current time
 	LARGE_INTEGER ClickTime = {};
 	QueryPerformanceCounter(&ClickTime);
-	LONGLONG TargetEndTicks = Counter.LastPresentTime.QuadPart;
-	LONGLONG FramesSinceLastCall = ((ClickTime.QuadPart - Counter.LastPresentTime.QuadPart - 1) / PreFrameTicks) + 1;
-	if (Counter.LastPresentTime.QuadPart == 0 || FramesSinceLastCall > 2)
+
+	LONGLONG TargetEndTicks = Counter.LastPresentTime.QuadPart + PerFrameTicks;
+
+	// First frame or if we fell behind, reset base time
+	if (Counter.LastPresentTime.QuadPart == 0 || ClickTime.QuadPart >= TargetEndTicks)
 	{
-		QueryPerformanceCounter(&Counter.LastPresentTime);
-		TargetEndTicks = Counter.LastPresentTime.QuadPart;
+		Counter.LastPresentTime.QuadPart = ClickTime.QuadPart;
+		return;
 	}
-	else
+
+	// Wait until target time
+	while (true)
 	{
-		TargetEndTicks += FramesSinceLastCall * PreFrameTicks;
-	}
-	
-	// Wait for time to expire
-	bool DoLoop;
-	do {
 		QueryPerformanceCounter(&ClickTime);
 		LONGLONG RemainingTicks = TargetEndTicks - ClickTime.QuadPart;
 
-		// Check if we still need to wait
-		DoLoop = RemainingTicks > 0;
+		if (RemainingTicks <= 0) break;
 
-		if (DoLoop)
-		{
-			// Busy wait until we reach the target time
-			BusyWaitYield(static_cast<DWORD>(RemainingTicks / TicksPerMS));
-		}
-	} while (DoLoop);
+		// Busy wait until we reach the target time
+		BusyWaitYield(static_cast<DWORD>(RemainingTicks / TicksPerMS));
+	}
 
-	// Update the last present time
+	// Store target time for next frame
 	Counter.LastPresentTime.QuadPart = TargetEndTicks;
 }
 

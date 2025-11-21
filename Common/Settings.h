376d18b22ee3e27e403782bda125c6936e740a2e
Murugo <49109252+Murugo@users.noreply.github.com>
2025-11-21 11:01:03 -0800

    Add patch to fix the labyrinth elevator sound (#1165)

--- Common/Settings.h
+++ Common/Settings.h
@@ -90,6 +90,7 @@
 	visit(ImproveStorageSupport, true) \
 	visit(IncreaseBlood, true) \
 	visit(IncreaseDrawDistance, true) \
+	visit(LabyrinthElevatorVolumeFix, true) \
 	visit(LegacyFixGPUAntiAliasing, false) \
 	visit(LightingFix, true) \
 	visit(LightingTransitionFix, true) \
@@ -255,6 +256,7 @@
 	visit(HookDirectInput) \
 	visit(HookDirectSound) \
 	visit(HookWndProc) \
+	visit(LabyrinthElevatorVolumeFix) \
 	visit(LetterSpacing) \
 	visit(LimitPerFrameFPS) \
 	visit(LoadModulesFromMemory) \

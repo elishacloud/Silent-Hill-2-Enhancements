c79b5cc3e032f3f35a99710e3152e50e3669d83b
Elisha Riedlinger <elisha@novicemail.com>
2026-02-08 14:45:26 -0800

    Add ReShade directly to base project files

--- ReShade/stb/stb_impl.c
+++ ReShade/stb/stb_impl.c
@@ -3,7 +3,12 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 
+#pragma warning(push)
+#pragma warning(disable : 4204 4244 4389)
+
 #include "stb_image.h"
 #include "stb_image_dds.h"
 #include "stb_image_write.h"
 #include "stb_image_resize.h"
+
+#pragma warning(pop)
